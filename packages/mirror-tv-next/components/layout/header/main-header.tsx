a6f03664a3741990a4d9c88882f4fdcef39eb3da
bibibobobi <smilingcat33@gmail.com>
2023-12-21 19:34:15 +0800

    chore: add GLOBAL_CACHE_SETTING for axios request

--- packages/mirror-tv-next/components/layout/header/main-header.tsx
+++ packages/mirror-tv-next/components/layout/header/main-header.tsx
@@ -1,16 +1,22 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import Image from 'next/image'
 import Link from 'next/link'
-import { HEADER_JSON_URL } from '~/constants/environment-variables'
+import {
+  GLOBAL_CACHE_SETTING,
+  HEADER_JSON_URL,
+} from '~/constants/environment-variables'
 import logoSrc from '~/public/icons/mnews-logo.svg'
 import styles from './main-header.module.css'
 
 export default async function MainHeader() {
-  let data
-
   try {
-    const response = await axios.get(HEADER_JSON_URL)
-    data = response.data
+    const response: AxiosResponse = await axios.get(HEADER_JSON_URL)
+    // Access the headers from Axios response
+    const headers = response.headers
+    // Set Cache-Control header in the headers object
+    headers['Cache-Control'] = GLOBAL_CACHE_SETTING
+
+    const data = response.data
     console.log(data)
   } catch (error) {
     const err = error as Error
