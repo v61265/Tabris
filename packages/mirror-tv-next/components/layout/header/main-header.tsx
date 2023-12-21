319b067c746d5597271274df0162fc9d5f8bf4c8
bibibobobi <smilingcat33@gmail.com>
2023-12-21 19:01:04 +0800

    chore: add axios

--- packages/mirror-tv-next/components/layout/header/main-header.tsx
+++ packages/mirror-tv-next/components/layout/header/main-header.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios'
 import Image from 'next/image'
 import Link from 'next/link'
+import { HEADER_JSON_URL } from '~/constants/environment-variables'
 import logoSrc from '~/public/icons/mnews-logo.svg'
 import styles from './main-header.module.css'
 
-export default function MainHeader(): JSX.Element {
+export default async function MainHeader() {
+  let data
+
+  try {
+    const response = await axios.get(HEADER_JSON_URL)
+    data = response.data
+    console.log(data)
+  } catch (error) {
+    const err = error as Error
+    console.error(JSON.stringify({ severity: 'ERROR', message: err.stack }))
+  }
+
   return (
     <header className={styles.header}>
       <Link href="/">
