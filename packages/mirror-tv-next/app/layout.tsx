8158f4654be14b059dcca72e4b13a35d5fb3d718
bibibobobi <smilingcat33@gmail.com>
2023-12-21 15:23:44 +0800

    chore(tv-next): add config and constant file

--- packages/mirror-tv-next/app/layout.tsx
+++ packages/mirror-tv-next/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
 import MainHeader from '~/components/layout/header/main-header'
+import { META_DESCRIPTION, SITE_TITLE } from '~/constants/constant'
 
 export const metadata: Metadata = {
-  title: '鏡新聞',
-  description:
-    '鏡電視股份有限公司創立「鏡新聞」，以多元、專業、深度、國際、藝文、弱勢為特色，期待提供給大家耳目一新的優質新聞內容，也歡迎閱聽人隨時給我們建議。',
+  title: SITE_TITLE,
+  description: META_DESCRIPTION,
 }
 
 export default function RootLayout({
