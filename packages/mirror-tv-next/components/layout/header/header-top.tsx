66531a6f323336738f6a6639e1dee86a9cefdba8
bibibobobi <smilingcat33@gmail.com>
2023-12-26 12:27:47 +0800

    chore(tv-next): move global css into styles folder

--- packages/mirror-tv-next/components/layout/header/header-top.tsx
+++ packages/mirror-tv-next/components/layout/header/header-top.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import type { Sponsor } from '~/graphql/query/sponsors'
 import logoSrc from '~/public/icons/mnews-logo.svg'
+import styles from './header-top.module.css'
 
 type HeaderTopProps = {
   sponsors: Sponsor[]
@@ -10,10 +11,10 @@ type HeaderTopProps = {
 export default function HeaderTop({ sponsors }: HeaderTopProps) {
   console.log(sponsors)
   return (
-    <>
+    <div className={styles.wrapper}>
       <Link href="/">
         <Image src={logoSrc} alt="mnews logo" priority />
       </Link>
-    </>
+    </div>
   )
 }
