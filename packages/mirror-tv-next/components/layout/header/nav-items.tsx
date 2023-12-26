9e16fd43adeb4956364b6ffe3584969adab680f2
bibibobobi <smilingcat33@gmail.com>
2023-12-26 21:01:06 +0800

    feat(tv-next): add nav-items visibleCategories

--- packages/mirror-tv-next/components/layout/header/nav-items.tsx
+++ packages/mirror-tv-next/components/layout/header/nav-items.tsx
@@ -1,56 +1,70 @@
 'use client'
 import Link from 'next/link'
-import { useState } from 'react'
-import styled from 'styled-components'
+import { useEffect, useState } from 'react'
 import type { Category } from '~/graphql/query/categories'
+import styles from './nav-items.module.css'
 
 type NavItemProps = {
   categories: Category[]
 }
 
-const ItemsWrapper = styled.div`
-  background-color: yellowgreen;
-  display: flex;
-  flex-wrap: wrap;
-  max-width: 1200px;
-`
-const Li = styled.li`
-  padding: 0 8px;
-`
-
-const RestOfTheCategories = styled.div`
-  display: ${({ show }) => (show ? 'flex' : 'none')};
-  flex-wrap: wrap;
-  width: 100%;
-`
-
 export default function NavItems({ categories }: NavItemProps) {
   console.log(categories)
 
   const [showRest, setShowRest] = useState(false)
-  const totalVisibleCategories = 9 // Number of categories initially visible
+  const [totalVisibleCategories, setTotalVisibleCategories] = useState(9)
 
   const handleSeeMoreClick = () => {
-    setShowRest((prevState) => !prevState) // Toggle the showRest state
+    setShowRest((prevState) => !prevState)
   }
 
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth
+      // Define your threshold for tablet width here, for example, 768px
+      const tabletWidthThreshold = 1200
+
+      if (width <= tabletWidthThreshold) {
+        setTotalVisibleCategories(5) // Set totalVisibleCategories to 5 for tablet
+      } else {
+        setTotalVisibleCategories(9) // Set totalVisibleCategories to 9 for PC
+      }
+    }
+
+    // Add event listener for resize to detect changes in viewport width
+    window.addEventListener('resize', handleResize)
+
+    // Call handleResize once initially to set the correct value on component mount
+    handleResize()
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <ItemsWrapper>
+    <div className={styles.itemWrapper}>
       {categories.slice(0, totalVisibleCategories).map((category) => (
-        <Li key={category.id}>
+        <li key={category.id} className={styles.li}>
           <Link href={`/category/${category.slug}`}>{category.name}</Link>
-        </Li>
+        </li>
       ))}
+      <li className={styles.li}>節目列表</li>
       {categories.length > totalVisibleCategories && (
-        <Li onClick={handleSeeMoreClick}>看更多</Li>
+        <li onClick={handleSeeMoreClick} className={styles.li}>
+          看更多
+        </li>
       )}
-      <RestOfTheCategories show={showRest}>
+      <div
+        className={`${styles.restOfCategories} ${showRest ? styles.show : ''}`}
+      >
         {categories.slice(totalVisibleCategories).map((category) => (
-          <Li key={category.id}>
+          <li key={category.id} className={styles.li}>
             <Link href={`/category/${category.slug}`}>{category.name}</Link>
-          </Li>
+          </li>
         ))}
-      </RestOfTheCategories>
-    </ItemsWrapper>
+      </div>
+    </div>
   )
 }
