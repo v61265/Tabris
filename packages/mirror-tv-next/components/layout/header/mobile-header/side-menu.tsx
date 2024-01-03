b041bfc6fc682d8c1d4f30d270acb10695384a3b
bibibobobi <smilingcat33@gmail.com>
2024-01-03 17:04:52 +0800

    feat(tv-next): add sponsors block on sidemenu

--- packages/mirror-tv-next/components/layout/header/mobile-header/side-menu.tsx
+++ packages/mirror-tv-next/components/layout/header/mobile-header/side-menu.tsx
@@ -1,9 +1,12 @@
 'use client'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
 import type { Category } from '~/graphql/query/categories'
 import type { Show } from '~/graphql/query/shows'
 import type { Sponsor } from '~/graphql/query/sponsors'
 import menuSrc from '~/public/icons/side-menu-icon.svg'
+import styles from './side-menu.module.css'
 
 type SideMenuProps = {
   categories: Category[]
@@ -16,10 +19,54 @@ export default function SideMenu({
   shows,
   sponsors,
 }: SideMenuProps) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
+
+  // Conditionally set the class based on isSidebarOpen
+  const sidebarWrapperClasses = `${styles.sidebarWrapper} ${
+    isSidebarOpen ? styles.sidebarOpen : ''
+  }`
+
   console.log(categories, shows, sponsors)
   return (
     <div>
-      <Image src={menuSrc} alt="menu icon" priority />
+      <button onClick={toggleSidebar} style={{ outline: 'none' }}>
+        <Image src={menuSrc} alt="menu icon" priority />
+      </button>
+      <div className={sidebarWrapperClasses}>
+        <div className={styles.sponsorsBlock}>
+          <div className={styles.sponsorsWrapper}>
+            {sponsors.map((sponsor) => {
+              return (
+                <div key={sponsor.id}>
+                  <Link href={`/topic/${sponsor.topic.slug}`}>
+                    <Image
+                      src={sponsor.logo.urlMobileSized}
+                      alt="Sponsor Logo"
+                      width={100}
+                      height={52}
+                      priority
+                      style={{
+                        width: 100,
+                        height: 52,
+                      }}
+                    />
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        {/* Sidebar content goes here */}
+        {/* You can map through categories, shows, sponsors, etc., to display content */}
+        {/* For example: */}
+        {categories.map((category) => (
+          <div key={category.id}>{category.name}</div>
+        ))}
+      </div>
     </div>
   )
 }
