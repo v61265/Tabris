cb30e2c77c8d799ace8cfaaef3fe2f474d142fd7
bibibobobi <smilingcat33@gmail.com>
2024-01-05 16:19:04 +0800

    feat(tv-next/apollo-client): create a new ApolloClient instance for every request

--- packages/mirror-tv-next/apollo-client.ts
+++ packages/mirror-tv-next/apollo-client.ts
@@ -1,21 +1,30 @@
-import { HttpLink } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { isServer } from '~/utils/common'
 
-import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc'
-import {
-  NextSSRApolloClient,
-  NextSSRInMemoryCache,
-} from '@apollo/experimental-nextjs-app-support/ssr'
 import { API_ENDPOINT } from './constants/config'
 
 // reference: https://www.apollographql.com/blog/how-to-use-apollo-client-with-next-js-13
-// @apollo/experimental-nextjs-app-support makes sure that we only instance the Apollo Client once per request,
+// makes sure that we only instance the Apollo Client once per request,
 // since Apollo Client’s cache is designed with a single user in mind, we recommend that your Next.js server instantiates a new cache for each SSR request, rather than reusing the same long-lived instance for multiple users’ data.
 
-export const { getClient } = registerApolloClient(() => {
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache(),
-    link: new HttpLink({
-      uri: API_ENDPOINT,
-    }),
-  })
-})
+let client: ApolloClient<any> | null = null
+
+export const getClient = () => {
+  // creat a new client if there's no existing one
+  // or if we are running on the server.
+  if (!client || isServer()) {
+    client = new ApolloClient({
+      link: new HttpLink({
+        uri: API_ENDPOINT,
+      }),
+      cache: new InMemoryCache(),
+      defaultOptions: {
+        query: {
+          fetchPolicy: 'no-cache',
+          errorPolicy: 'all',
+        },
+      },
+    })
+  }
+  return client
+}
