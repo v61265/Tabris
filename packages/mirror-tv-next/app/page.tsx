7ffce395cd95c11886b45f733e012d28a43c93b0
bibibobobi <smilingcat33@gmail.com>
2024-01-10 17:04:09 +0800

    feat(tv-next): add Flash News Mock data

--- packages/mirror-tv-next/app/page.tsx
+++ packages/mirror-tv-next/app/page.tsx
@@ -1,9 +1,13 @@
+import UiMobFlashNews from '~/components/flash-news/ui-mob-flash-news'
 import styles from '~/styles/pages/page.module.scss'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <p>Hello, world.</p>
-    </main>
+    <>
+      <main className={styles.main}>
+        <p>Hello, world.</p>
+      </main>
+      <UiMobFlashNews />
+    </>
   )
 }
--- packages/mirror-tv-next/components/layout/header/header-bottom.tsx
+++ packages/mirror-tv-next/components/layout/header/header-bottom.tsx
@@ -1,6 +1,9 @@
+import UiPcFlashNews from '~/components/flash-news/ui-pc-flash-news'
+
 export default function HeaderBottom(): JSX.Element {
   return (
     <>
+      <UiPcFlashNews />
       <p>HeaderBottom</p>
     </>
   )
