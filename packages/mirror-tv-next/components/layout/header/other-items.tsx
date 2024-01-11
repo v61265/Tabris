e6a5ec622aa125178d547105e60f7a18ab3d554e
bibibobobi <smilingcat33@gmail.com>
2024-01-11 14:57:12 +0800

    feat(tv-next/header): add social icons

--- packages/mirror-tv-next/components/layout/header/other-items.tsx
+++ packages/mirror-tv-next/components/layout/header/other-items.tsx
@@ -1,16 +1,45 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import xSrc from 'public/icons/icon-x.svg'
+import { HEADER_BOTTOM_LINKS } from '~/constants/constant'
+import fbSrc from '~/public/icons/icon-fb.svg'
+import igSrc from '~/public/icons/icon-ig.svg'
+import lineSrc from '~/public/icons/icon-line.svg'
 import styles from '~/styles/components/layout/header/other-items.module.scss'
 
 export default function OtherItems(): JSX.Element {
   return (
     <div className={styles.otherItemsWrapper}>
       <ul className={styles.items}>
-        <li>公評人專區</li>
-        <li>鏡主播</li>
-        <li>關於我們</li>
-        <li>fb</li>
-        <li>line</li>
-        <li>ig</li>
-        <li>x</li>
+        <li>
+          <Link href={HEADER_BOTTOM_LINKS.ombuds}>公評人專區</Link>
+        </li>
+        <li>
+          <Link href={HEADER_BOTTOM_LINKS.anchorperson}>鏡主播</Link>
+        </li>
+        <li>
+          <Link href={HEADER_BOTTOM_LINKS.about}>關於我們</Link>
+        </li>
+        <li className={styles.icon}>
+          <Link href={HEADER_BOTTOM_LINKS.fb}>
+            <Image src={fbSrc} alt="facebook icon" />
+          </Link>
+        </li>
+        <li className={styles.icon}>
+          <Link href={HEADER_BOTTOM_LINKS.line}>
+            <Image src={lineSrc} alt="line icon" />
+          </Link>
+        </li>
+        <li className={styles.icon}>
+          <Link href={HEADER_BOTTOM_LINKS.ig}>
+            <Image src={igSrc} alt="instagram icon" />
+          </Link>
+        </li>
+        <li className={styles.icon}>
+          <Link href={HEADER_BOTTOM_LINKS.x}>
+            <Image src={xSrc} alt="x(former twitter) icon" />
+          </Link>
+        </li>
       </ul>
     </div>
   )
