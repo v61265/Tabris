58be183e624ba5b00cc013842a85b8c392a8e1df
bibibobobi <smilingcat33@gmail.com>
2024-01-12 13:01:27 +0800

    refactor(tv-next): rearrange flash news data flow

--- packages/mirror-tv-next/components/layout/header/header-bottom.tsx
+++ packages/mirror-tv-next/components/layout/header/header-bottom.tsx
@@ -1,11 +1,12 @@
-import UiPcFlashNews from '~/components/flash-news/ui-pc-flash-news'
+// import UiPcFlashNews from '~/components/flash-news/ui-pc-flash-news'
+import MainFlashNews from '~/components/flash-news/main-flash-news'
 import OtherItems from '~/components/layout/header/other-items'
 import styles from '~/styles/components/layout/header/header-bottom.module.scss'
 
 export default function HeaderBottom(): JSX.Element {
   return (
     <div className={styles.headerBottomWrapper}>
-      <UiPcFlashNews />
+      <MainFlashNews />
       <OtherItems />
     </div>
   )
