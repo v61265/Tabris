a4867804101edaa2f1033c7c3e54db662ea19c3c
bibibobobi <smilingcat33@gmail.com>
2024-01-26 12:14:15 +0800

    feat(tv-next): fetch schedule data and add type Schedule

--- packages/mirror-tv-next/types/common.ts
+++ packages/mirror-tv-next/types/common.ts
@@ -2,3 +2,21 @@ export type FlashNews = {
   slug: string
   name: string
 }
+
+export type Schedule = {
+  'Channel ID': string
+  Year: string
+  WeekDay: string
+  'Start Time(hh)': string
+  'Start Time(mm)': string
+  Duration: string
+  Programme: string
+  'Programme(en)': string
+  'ep no': string
+  'ep name': string
+  'season no': string
+  Class: string
+  TxCategory: string
+  Month: string
+  Day: string
+}
