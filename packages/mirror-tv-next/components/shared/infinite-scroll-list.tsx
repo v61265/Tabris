60bc5daed249f7f04e1da7067726212c54fbc727
YiHsuan <v61265@gmail.com>
2024-02-19 13:48:55 +0800

    refactor(tv/infinite-scroll-list): set const totalPage and remove debugger

--- packages/mirror-tv-next/components/shared/infinite-scroll-list.tsx
+++ packages/mirror-tv-next/components/shared/infinite-scroll-list.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react'
-import styles from '~/styles/components/shared/infinite-scroll-list.module.scss'
 
 type Children<ItemType> = (renderList: ItemType[]) => JSX.Element
 
@@ -26,19 +25,15 @@ export default function InfiniteScrollList<ItemType>({
   const [renderCount, setRenderCount] = useState(pageSize)
   const renderList = dataList.slice(0, renderCount)
   const [isLoading, setIsLoading] = useState(false)
-  const isTestMode = false
+
+  const totalPage = Math.ceil(totalCount / pageSize)
 
   const hasUnFetchedData = useMemo(
-    () =>
-      !(
-        renderCount >= dataList.length &&
-        fetchedPage >= Math.ceil(totalCount / pageSize)
-      ),
+    () => !(renderCount >= dataList.length && fetchedPage >= totalPage),
     [dataList.length, renderCount, fetchedPage, totalCount, pageSize]
   )
   const isNotEnoughToRender =
-    fetchedPage < Math.ceil(totalCount / pageSize) &&
-    dataList.length - renderCount <= pageSize
+    fetchedPage < totalPage && dataList.length - renderCount <= pageSize
 
   const handleLoadMore = useCallback(() => {
     if (isLoading) {
@@ -89,39 +84,6 @@ export default function InfiniteScrollList<ItemType>({
 
   return (
     <div>
-      {isTestMode && (
-        <div className={styles.test}>
-          <p>資料顯示狀態</p>
-          <ul>
-            <li>一次無限滾動應顯示 {pageSize} 筆</li>
-            <li>目前取得的資料共 {dataList.length} 筆</li>
-            <li>目前顯示的資料共 {renderList.length} 筆</li>
-            <li>
-              {isNotEnoughToRender
-                ? '取得的資料不足以顯示，需另外發request'
-                : '取得的資料足以顯示，不需要發request'}
-            </li>
-            <li>最終應顯示 {totalCount} 筆</li>
-          </ul>
-          --
-          <p>發request狀態</p>
-          <ul>
-            <li>目前已發request {fetchedPage} 次</li>
-            <li>最終應發request {Math.ceil(totalCount / pageSize)} 次</li>
-          </ul>
-          --
-          <p>loading狀態</p>
-          <ul>
-            <li>正在發request中：{isLoading ? '是' : '否'}</li>
-            <li>
-              {hasUnFetchedData
-                ? '仍有資料未被取得，開啟無限滾動功能'
-                : '所有資料皆已被取得且顯示，關閉無限滾動功能'}
-            </li>
-            <li></li>
-          </ul>
-        </div>
-      )}
       {children(renderList)}
 
       <div ref={loaderRef}>{hasUnFetchedData && loader}</div>
