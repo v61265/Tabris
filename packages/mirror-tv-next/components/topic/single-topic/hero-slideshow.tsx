33b23d0fee2184c0265d1aab49188288f2ddfefa
bibibobobi <smilingcat33@gmail.com>
2024-02-23 15:17:23 +0800

    feat(tv-next): replace next/image with @readr-media/react-image

--- packages/mirror-tv-next/components/topic/single-topic/hero-slideshow.tsx
+++ packages/mirror-tv-next/components/topic/single-topic/hero-slideshow.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from '@readr-media/react-image'
 import type { HeroImage, Slideshow } from '~/graphql/query/topic'
 import styles from '~/styles/components/topic/single-topic/hero-slideshow.module.scss'
 import { formateHeroImage } from '~/utils'
@@ -17,7 +18,24 @@ export default function HeroSlideshow({
   console.log(heroImage, title, slideshow)
 
   const formattedHeroImage = formateHeroImage(heroImage)
-  console.log({ formattedHeroImage })
+ 
 
-  return <section className={styles.sectionWrapper}>Slideshow</section>
+  return (
+    <section className={styles.sectionWrapper}>
+      <Image
+        images={formattedHeroImage}
+        alt={title}
+        // loadingImage="/images/loading.svg"
+        defaultImage="/images/image-default.jpg"
+        rwd={{
+          mobile: '100vw',
+          tablet: '100vw',
+          laptop: '100vw',
+          desktop: '100vw',
+          default: '100vw',
+        }}
+        priority
+      />
+    </section>
+  )
 }
