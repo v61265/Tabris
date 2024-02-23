454a3c8cf3d6231526f78f2d65e35492251a09d6
bibibobobi <smilingcat33@gmail.com>
2024-02-23 20:56:08 +0800

    feat(tv-next): add hero-slidshow swiper component

--- packages/mirror-tv-next/components/topic/single-topic/hero-slideshow.tsx
+++ packages/mirror-tv-next/components/topic/single-topic/hero-slideshow.tsx
@@ -1,8 +1,24 @@
 'use client'
 import Image from '@readr-media/react-image'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import type { HeroImage, Slideshow } from '~/graphql/query/topic'
 import styles from '~/styles/components/topic/single-topic/hero-slideshow.module.scss'
+import '~/styles/components/topic/single-topic/swiper-custom-styles.scss'
 import { formateHeroImage } from '~/utils'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+// import required modules
+import {
+  Autoplay,
+  Keyboard,
+  Mousewheel,
+  Navigation,
+  Pagination,
+} from 'swiper/modules'
 
 type HeroSlideshowProps = {
   heroImage: HeroImage
@@ -18,24 +34,66 @@ export default function HeroSlideshow({
   console.log(heroImage, title, slideshow)
 
   const formattedHeroImage = formateHeroImage(heroImage)
- 
 
   return (
     <section className={styles.sectionWrapper}>
-      <Image
-        images={formattedHeroImage}
-        alt={title}
-        // loadingImage="/images/loading.svg"
-        defaultImage="/images/image-default.jpg"
-        rwd={{
-          mobile: '100vw',
-          tablet: '100vw',
-          laptop: '100vw',
-          desktop: '100vw',
-          default: '100vw',
-        }}
-        priority
-      />
+      <div className={styles.imageContainer}>
+        <div className={styles.swiperContainer}>
+          <Swiper
+            cssMode={true}
+            slidesPerView={1}
+            spaceBetween={30}
+            navigation={true}
+            loop={true}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Autoplay, Navigation, Pagination, Mousewheel, Keyboard]}
+            className={styles.swiper}
+          >
+            <SwiperSlide className={styles.swiperSlide}>
+              <div className={styles.slideImageContainer}>
+                <Image
+                  images={formattedHeroImage}
+                  alt={title}
+                  loadingImage="/images/loading.svg"
+                  defaultImage="/images/image-default.jpg"
+                  rwd={{
+                    mobile: '100vw',
+                    tablet: '100vw',
+                    laptop: '100vw',
+                    desktop: '100vw',
+                    default: '100vw',
+                  }}
+                  priority
+                />
+                <div className={styles.articleTitle}>一些字一些字</div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styles.swiperSlide}>Slide 2</SwiperSlide>
+            <SwiperSlide className={styles.swiperSlide}>Slide 3</SwiperSlide>
+          </Swiper>
+        </div>
+        <Image
+          images={formattedHeroImage}
+          alt={title}
+          defaultImage="/images/image-default.jpg"
+          rwd={{
+            mobile: '100vw',
+            tablet: '100vw',
+            laptop: '100vw',
+            desktop: '100vw',
+            default: '100vw',
+          }}
+          priority
+        />
+      </div>
     </section>
   )
 }
