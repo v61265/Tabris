62f7fb2fca521d1de437a998c6c9fd822eeb2b09
YiHsuan <v61265@gmail.com>
2024-02-26 13:52:56 +0800

    refactor(ui-heading-bordered): remove cache setting

--- packages/mirror-tv-next/components/shared/ui-heading-bordered.tsx
+++ packages/mirror-tv-next/components/shared/ui-heading-bordered.tsx
@@ -1,7 +1,4 @@
 import styles from '~/styles/components/shared/ui-heading-bordered.module.scss'
-import { GLOBAL_CACHE_SETTING } from '~/constants/environment-variables'
-
-export const revalidate = GLOBAL_CACHE_SETTING
 
 type UiHeadingBorderedProps = {
   title: string
