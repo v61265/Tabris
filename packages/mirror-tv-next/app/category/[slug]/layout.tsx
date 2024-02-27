d203e78607d33b575382583d533aa93bb36b8f1b
YiHsuan <v61265@gmail.com>
2024-02-27 12:02:18 +0800

    refactor: move aside to layout

---
+++ packages/mirror-tv-next/app/category/[slug]/layout.tsx
@@ -0,0 +1,139 @@
+import { getClient } from '~/apollo-client'
+import errors from '@twreporter/errors'
+import styles from '~/styles/pages/category-layout.module.scss'
+import { getLatestPosts, PostCardItem } from '~/graphql/query/posts'
+import { formatArticleCard, FormattedPostCard } from '~/utils'
+import UiListPostsAside from '~/components/shared/ui-list-posts-aside'
+import {
+  POPULAR_POSTS_URL,
+  GLOBAL_CACHE_SETTING,
+} from '~/constants/environment-variables'
+import { ApolloQueryResult } from '@apollo/client'
+
+export default async function CategoryPageLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  let latestPosts: FormattedPostCard[] = []
+  let popularPosts: FormattedPostCard[] = []
+
+  const client = getClient()
+
+  const fetchLatestPosts = () =>
+    client.query<{
+      allPosts: PostCardItem[]
+    }>({
+      query: getLatestPosts,
+      variables: {
+        first: 5,
+      },
+    })
+
+  const fetchPopularPosts = () =>
+    fetch(POPULAR_POSTS_URL, {
+      next: { revalidate: GLOBAL_CACHE_SETTING },
+    }).then((res) => res.json())
+
+  const responses = await Promise.allSettled([
+    fetchLatestPosts(),
+    fetchPopularPosts(),
+  ])
+
+  const handledResponse = <
+    T extends Record<string, unknown>,
+    U extends PromiseSettledResult<unknown>,
+    V
+  >(
+    response: U,
+    callback: (value: T | undefined) => V
+  ): V | undefined => {
+    if (response.status === 'fulfilled') {
+      return callback(response.value)
+    } else if (response.status === 'rejected') {
+      const { graphQLErrors, clientErrors, networkError } = response.reason
+      const annotatingError = errors.helpers.wrap(
+        response.reason,
+        'UnhandledError',
+        'Error occurs while fetching category data in category page'
+      )
+
+      console.log(
+        JSON.stringify({
+          severity: 'ERROR',
+          message: errors.helpers.printAll(
+            annotatingError,
+            {
+              withStack: true,
+              withPayload: true,
+            },
+            0,
+            0
+          ),
+          debugPayload: {
+            graphQLErrors,
+            clientErrors,
+            networkError,
+          },
+        })
+      )
+      return callback(undefined)
+    }
+  }
+
+  latestPosts =
+    handledResponse<
+      { data: { allPosts: PostCardItem[] } },
+      PromiseSettledResult<ApolloQueryResult<{ allPosts: PostCardItem[] }>>,
+      FormattedPostCard[]
+    >(responses[0], (latestPostsData) => {
+      return (
+        latestPostsData?.data?.allPosts?.map((post) =>
+          formatArticleCard(post)
+        ) ?? []
+      )
+    }) ?? []
+
+  popularPosts =
+    handledResponse<
+      {
+        report: PostCardItem[]
+        start_date: string
+        end_date: string
+        generate_time: string
+      },
+      PromiseSettledResult<
+        () => Promise<{
+          report: PostCardItem[]
+          start_date: string
+          end_date: string
+          generate_time: string
+        }>
+      >,
+      FormattedPostCard[]
+    >(responses[1], (popularPostsData) => {
+      return (
+        popularPostsData?.report.map((post: PostCardItem) =>
+          formatArticleCard(post)
+        ) ?? []
+      )
+    }) ?? []
+
+  return (
+    <main className={styles.category}>
+      {children}
+      <aside className={styles.aside}>
+        <UiListPostsAside
+          listTitle="熱門新聞"
+          page="category"
+          listData={popularPosts}
+        />
+        <UiListPostsAside
+          listTitle="最新新聞"
+          page="category"
+          listData={latestPosts}
+        />
+      </aside>
+    </main>
+  )
+}
--- packages/mirror-tv-next/app/category/[slug]/page.tsx
+++ packages/mirror-tv-next/app/category/[slug]/page.tsx
@@ -6,17 +6,13 @@ import type { Metadata } from 'next'
 import { getClient } from '~/apollo-client'
 import errors from '@twreporter/errors'
 import { fetchCategoryBySlug, Category } from '~/graphql/query/category'
-import { getLatestPosts, PostCardItem } from '~/graphql/query/posts'
 import UiHeadingBordered from '~/components/shared/ui-heading-bordered'
 import styles from '~/styles/pages/category.module.scss'
 import { fetchPostsItems } from '~/components/category/action'
 import UiFeaturePost from '~/components/category/ui-feature-post'
 import PostsListManager from '~/components/category/posts-list-manager'
 import { formatArticleCard, FormattedPostCard } from '~/utils'
-import axios from 'axios'
-import UiListPostsAside from '~/components/shared/ui-list-posts-aside'
 import { notFound } from 'next/navigation'
-import { POPULAR_POSTS_URL } from '~/constants/environment-variables'
 
 export const revalidate = GLOBAL_CACHE_SETTING
 
@@ -76,140 +72,53 @@ export default async function CategoryPage({
 }) {
   const PAGE_SIZE = 12
   let categoryData: Category = { name: '', slug: '' }
-  let latestPosts: FormattedPostCard[] = []
   let postsCount: number = 0
   let categoryPosts: FormattedPostCard[] = []
-  let popularPosts: FormattedPostCard[] = []
-
-  const client = getClient()
 
   categoryData = await fetchCategoryData(params.slug)
   if (!categoryData.name) return notFound()
 
-  const fetchInitPostsList = () => {
-    return fetchPostsItems({
+  try {
+    const { allPosts, _allPostsMeta } = await fetchPostsItems({
       page: 0,
       categorySlug: categoryData.slug,
       pageSize: PAGE_SIZE,
       isWithCount: true,
     })
+    postsCount = _allPostsMeta?.count ?? 0
+    categoryPosts = allPosts.map((post) => formatArticleCard(post)) ?? []
+  } catch (err) {
+    const annotatingError = errors.helpers.wrap(
+      err,
+      'UnhandledError',
+      'Error occurs while fetching data for category for category page'
+    )
+
+    console.error(
+      JSON.stringify({
+        severity: 'ERROR',
+        message: errors.helpers.printAll(annotatingError, {
+          withStack: false,
+          withPayload: true,
+        }),
+      })
+    )
   }
-
-  const fetchLatestPosts = () =>
-    client.query<{
-      allPosts: PostCardItem[]
-    }>({
-      query: getLatestPosts,
-      variables: {
-        first: 5,
-      },
-    })
-
-  const fetchPopularPosts = () => axios.get(POPULAR_POSTS_URL)
-
-  const responses = await Promise.allSettled([
-    fetchInitPostsList(),
-    fetchLatestPosts(),
-    fetchPopularPosts(),
-  ])
-
-  const handledResponses = responses.map((response) => {
-    if (response.status === 'fulfilled') {
-      if ('data' in response.value) {
-        // handle gql requests
-        return response.value.data
-      }
-      return response.value
-    } else if (response.status === 'rejected') {
-      const { graphQLErrors, clientErrors, networkError } = response.reason
-      const annotatingError = errors.helpers.wrap(
-        response.reason,
-        'UnhandledError',
-        'Error occurs while fetching category data in category page'
-      )
-
-      console.log(
-        JSON.stringify({
-          severity: 'ERROR',
-          message: errors.helpers.printAll(
-            annotatingError,
-            {
-              withStack: true,
-              withPayload: true,
-            },
-            0,
-            0
-          ),
-          debugPayload: {
-            graphQLErrors,
-            clientErrors,
-            networkError,
-          },
-        })
-      )
-      return []
-    }
-  })
-
-  const categoryPostsData = handledResponses[0] ?? {
-    allPosts: [],
-    _allPostsMeta: { count: 0 },
-  }
-  postsCount =
-    '_allPostsMeta' in categoryPostsData
-      ? categoryPostsData?._allPostsMeta?.count
-      : 0
-  categoryPosts =
-    'allPosts' in categoryPostsData
-      ? categoryPostsData?.allPosts.map((post: PostCardItem) =>
-          formatArticleCard(post)
-        )
-      : []
-
-  const latestPostsData = handledResponses[1] ?? { allPosts: [] }
-  latestPosts =
-    'allPosts' in latestPostsData
-      ? latestPostsData.allPosts.map((post: PostCardItem) =>
-          formatArticleCard(post)
-        )
-      : []
-
-  const popularPostsData = handledResponses[2] ?? { report: [] }
-  popularPosts =
-    'report' in popularPostsData
-      ? popularPostsData.report.map((post: PostCardItem) =>
-          formatArticleCard(post)
-        )
-      : []
 
   return (
-    <section className={styles.category}>
-      <main className={styles.main}>
-        <UiHeadingBordered title={categoryData.name} />
-        {postsCount !== 0 && (
-          <>
-            <UiFeaturePost post={categoryPosts[0]} />
-            <PostsListManager
-              categorySlug={categoryData.slug}
-              pageSize={PAGE_SIZE}
-              postsCount={postsCount}
-              initPostsList={categoryPosts.slice(1)}
-            />
-          </>
-        )}
-      </main>
-      <aside className={styles.aside}>
-        <UiListPostsAside
-          listTitle="熱門新聞"
-          page="category"
-          listData={popularPosts}
-        />
-        <UiListPostsAside
-          listTitle="最新新聞"
-          page="category"
-          listData={latestPosts}
-        />
-      </aside>
+    <section className={styles.postsList}>
+      <UiHeadingBordered title={categoryData.name} />
+      {postsCount !== 0 && (
+        <>
+          <UiFeaturePost post={categoryPosts[0]} />
+          <PostsListManager
+            categorySlug={categoryData.slug}
+            pageSize={PAGE_SIZE}
+            postsCount={postsCount}
+            initPostsList={categoryPosts.slice(1)}
+          />
+        </>
+      )}
     </section>
   )
 }
