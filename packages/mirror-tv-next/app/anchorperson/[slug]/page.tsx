b48a32d90b7439583a67c919d995f1a53e7c2781
bibibobobi <smilingcat33@gmail.com>
2024-03-27 11:31:10 +0800

    feat(tv-next): generate metadata for single anchor page

--- packages/mirror-tv-next/app/anchorperson/[slug]/page.tsx
+++ packages/mirror-tv-next/app/anchorperson/[slug]/page.tsx
@@ -1,12 +1,79 @@
 import errors from '@twreporter/errors'
+import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 import { getClient } from '~/apollo-client'
 import AnchorImg from '~/components/anchorperson/anchor-img'
 import SocialIcon from '~/components/anchorperson/social-icon'
+import { META_DESCRIPTION, SITE_TITLE } from '~/constants/constant'
+import {
+  GLOBAL_CACHE_SETTING,
+  SITE_URL,
+} from '~/constants/environment-variables'
 import type { SingleAnchor } from '~/graphql/query/contact'
 import { fetchContactBySlug } from '~/graphql/query/contact'
 import styles from '~/styles/pages/single-anchorperson-page.module.scss'
 import { handleApiData } from '~/utils'
+
+export const revalidate = GLOBAL_CACHE_SETTING
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}): Promise<Metadata> {
+  const client = getClient()
+  let singleAnchor: SingleAnchor | null = null
+
+  try {
+    const response = await client.query({
+      query: fetchContactBySlug,
+      variables: {
+        slug: params.slug,
+      },
+    })
+    const data = response.data
+    singleAnchor = data.allContacts[0]
+
+    if (!singleAnchor) {
+      const annotatingError = errors.helpers.wrap(
+        new Error('Anchor not found'),
+        'UnhandledError',
+        'Error occurs while fetching data for single anchor page'
+      )
+
+      console.error(
+        JSON.stringify({
+          severity: 'ERROR',
+          message: errors.helpers.printAll(annotatingError, {
+            withStack: false,
+            withPayload: true,
+          }),
+        })
+      )
+      throw annotatingError
+    }
+  } catch (err) {
+    console.error(err)
+    notFound()
+  }
+
+  const bio = handleApiData(singleAnchor.bioApiData)
+  const description = bio[0]?.content[0]?.slice(0, 20)
+
+  return {
+    metadataBase: new URL(`https://${SITE_URL}`),
+    title: `${singleAnchor?.name} - 鏡新聞`,
+    description: description !== '' ? description : META_DESCRIPTION,
+    openGraph: {
+      title: `${singleAnchor?.name} - 鏡新聞`,
+      description: description !== '' ? description : META_DESCRIPTION,
+      siteName: SITE_TITLE,
+      images:
+        singleAnchor?.showhostImg?.urlMobileSized ??
+        '/images/image-default.jpg',
+    },
+  }
+}
 
 export default async function singleAnchor({
   params,
