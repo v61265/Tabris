5f2a550ae3f411ae8c03445128ecff18e3b579d4
bibibobobi <smilingcat33@gmail.com>
2024-04-06 00:56:39 +0800

    feat(tv-next): add gpt ads script to root layout

--- packages/mirror-tv-next/app/layout.tsx
+++ packages/mirror-tv-next/app/layout.tsx
@@ -1,6 +1,7 @@
 import { GoogleTagManager } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 import { Noto_Sans } from 'next/font/google'
+import Script from 'next/script'
 import Footer from '~/components/layout/footer'
 import MainHeader from '~/components/layout/header/main-header'
 import { META_DESCRIPTION, SITE_TITLE } from '~/constants/constant'
@@ -29,6 +30,43 @@ export default function RootLayout({
   return (
     <html lang="ch" className={`${noto_sans.variable} ${noto_sans.variable}`}>
       <GoogleTagManager gtmId={GTM_ID} />
+      <Script
+        async
+        src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
+      />
+      <Script id="gpt-setup">
+        {`
+        window.googletag = window.googletag || {cmd: []};
+        window.googletag.cmd.push(() => {
+          /**
+           * Do not use lazy loading with SRA.
+           *
+           * With lazy loading in SRA,
+           * GPT will fetching multiple ads at the same time,
+           * which cause the call for the first ad and all other ad slots is made.
+           * https://developers.google.com/doubleclick-gpt/reference#googletag.PubAdsService_enableSingleRequest
+           */
+          // window.googletag.pubads().enableSingleRequest()
+
+          window.googletag.pubads().enableLazyLoad({
+            // Fetch slots within 1.5 viewports.
+            fetchMarginPercent: 150,
+
+            // Render slots within 1 viewports.
+            renderMarginPercent: 100,
+
+            /**
+             * Double the above values on mobile, where viewports are smaller
+             * and users tend to scroll faster.
+             */
+            mobileScaling: 2.0,
+          })
+          window.googletag.pubads().collapseEmptyDivs()
+          window.googletag.enableServices()
+
+          
+        })`}
+      </Script>
       <body>
         <>
           <MainHeader />
