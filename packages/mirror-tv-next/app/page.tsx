683b856c99557b6ac127c5802709aeea029bcb24
bibibobobi <smilingcat33@gmail.com>
2024-04-07 12:50:41 +0800

    feat(tv-next): add test gpt ads demo

--- packages/mirror-tv-next/app/page.tsx
+++ packages/mirror-tv-next/app/page.tsx
@@ -1,5 +1,8 @@
+import dynamic from 'next/dynamic'
 import MainFlashNews from '~/components/flash-news/main-flash-news'
 import styles from '~/styles/pages/page.module.scss'
+
+const GPTAd = dynamic(() => import('~/components/ads/gpt/gpt-ad'))
 
 export default function Home() {
   return (
@@ -8,6 +11,11 @@ export default function Home() {
         <MainFlashNews />
       </div>
       <p>Hello, world.</p>
+      {/* Test GPT ADs */}
+      <div>
+        <GPTAd pageKey="story" adKey="PC_R2" />
+        <GPTAd pageKey="all" adKey="PC_HD" />
+      </div>
     </main>
   )
 }
