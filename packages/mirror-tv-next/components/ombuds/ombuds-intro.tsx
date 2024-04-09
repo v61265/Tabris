2e4bd95efbb5dc8454976aed403b0874a5c03cc3
MirrorMedia <mirrormedia@MirrorMedias-MacBook-Pro.local>
2024-04-09 13:48:21 +0800

    feat(tv-next/ombuds): add report-issue section to ombuds-intro component

--- packages/mirror-tv-next/components/ombuds/ombuds-intro.tsx
+++ packages/mirror-tv-next/components/ombuds/ombuds-intro.tsx
@@ -5,29 +5,60 @@ import Link from 'next/link'
 export default function ombudsIntro() {
   return (
     <section className={styles.ombudsIntroContent}>
-      <h2 className={styles.ombudsTitle}>外部公評人翁秀琪</h2>
-      <div className={styles.ombudsIntroWrapper}>
-        <div className={styles.videoWrapper}>
-          <ArticleContentVideo
-            videoSrc={
-              'https://statics.mnews.tw/assets/videos/ckrvhg947002n10o4b2wvcpph.mp4'
-            }
-            autoPlay={true}
-            loop={true}
-            muted={true}
-          />
-        </div>
-
-        <article className={styles.introArticle}>
-          <p className={styles.para}>
-            翁秀琪是德國曼茵茲大學大眾傳播學博士，在傳播學術界擔任教職逾三十年。2021年1月1日起，擔任鏡電視新聞台外部公評人。主要工作在確保新聞內容符合正確、平衡、公平和好的品味，並代表閱聽人的利益，處理公眾申訴案件，扮演閱聽眾和電視台之間的溝通橋梁。外部公評人獨立於鏡電視新聞台，直接向董事會負責，任期三年，得連任。
-          </p>
-          <div className={styles.linkWrapper}>
-            <Link href="/story/biography" target="_blank">
-              了解更多
-            </Link>
+      <h1>外部公評人翁秀琪</h1>
+      <div className={styles.mainIntroWrapper}>
+        <section className={styles.ombudsIntroWrapper}>
+          <div className={styles.videoWrapper}>
+            <ArticleContentVideo
+              videoSrc={
+                'https://statics.mnews.tw/assets/videos/ckrvhg947002n10o4b2wvcpph.mp4'
+              }
+              autoPlay={true}
+              loop={true}
+              muted={true}
+            />
           </div>
-        </article>
+
+          <article className={styles.introArticle}>
+            <p>
+              翁秀琪是德國曼茵茲大學大眾傳播學博士，在傳播學術界擔任教職逾三十年。2021年1月1日起，擔任鏡電視新聞台外部公評人。主要工作在確保新聞內容符合正確、平衡、公平和好的品味，並代表閱聽人的利益，處理公眾申訴案件，扮演閱聽眾和電視台之間的溝通橋梁。外部公評人獨立於鏡電視新聞台，直接向董事會負責，任期三年，得連任。
+            </p>
+            <div className={styles.linkWrapper}>
+              <Link href="/story/biography" target="_blank">
+                了解更多
+              </Link>
+            </div>
+          </article>
+        </section>
+
+        <section className={styles.reportIssue}>
+          <h3>我要向公評人申訴</h3>
+          <div className={styles.contentWrapper}>
+            <div className={styles.mainContent}>
+              <p>
+                如果您對於我們的新聞內容有意見，例如：事實錯誤、侵害人權，或違反新聞倫理等，請按下方的向公評人申訴鍵。
+              </p>
+              <Link
+                href="/story/complaint"
+                target="_blank"
+                className={styles.reportLink}
+              >
+                向公評人申訴
+              </Link>
+            </div>
+            <div className={styles.contact}>
+              <p>客服事項請打客服專線：</p>
+              <p className={styles.tel}>（02）7752-5678</p>
+              <p>或洽客服信箱</p>
+              <Link
+                href="mailto:mnews.cs@mnews.com.tw"
+                className={styles.reportMail}
+              >
+                mnews.cs@mnews.com.tw
+              </Link>
+            </div>
+          </div>
+        </section>
       </div>
     </section>
   )
