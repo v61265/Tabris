c3682cc33d0fb6c16d5f7beaeb122dd3ae5fdbed
MirrorMedia <mirrormedia@MirrorMedias-MacBook-Pro.local>
2024-04-16 11:34:46 +0800

    feat(tv-next/ombuds): add ombuds server action

---
+++ packages/mirror-tv-next/components/ombuds/action.tsx
@@ -0,0 +1,61 @@
+'use server'
+import { getClient } from '~/apollo-client'
+import { fetchOmbudsPostsByCategorySlug } from '~/graphql/query/ombuds'
+import { Post } from '~/graphql/query/ombuds'
+import errors from '@twreporter/errors'
+
+type FetchOmbudsPostsProps = {
+  page: number
+  pageSize: number
+  slug: string[]
+  isWithCount: boolean
+}
+
+type OmbudsResponse = {
+  data: {
+    allPosts: Post[]
+    _allPostsMeta?: {
+      count: number
+    }
+  }
+}
+
+async function fetchOmbudsPosts({
+  page,
+  pageSize,
+  slug,
+  isWithCount,
+}: FetchOmbudsPostsProps): Promise<{ items: Post[] }> {
+  const client = getClient()
+  try {
+    const { data } = await client.query<OmbudsResponse>({
+      query: fetchOmbudsPostsByCategorySlug,
+      variables: {
+        first: pageSize,
+        skip: (page - 1) * pageSize,
+        filteredSlug: slug,
+        withCount: isWithCount,
+      },
+    })
+    return { items: data.data.allPosts }
+  } catch (err) {
+    const annotatingError = errors.helpers.wrap(
+      err,
+      'UnhandledError',
+      'Error occurs while fetching ombuds news'
+    )
+
+    console.error(
+      JSON.stringify({
+        severity: 'ERROR',
+        message: errors.helpers.printAll(annotatingError, {
+          withStack: false,
+          withPayload: true,
+        }),
+      })
+    )
+    throw annotatingError
+  }
+}
+
+export { fetchOmbudsPosts }
