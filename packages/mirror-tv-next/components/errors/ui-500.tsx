150cfa7dae6a3fc51c563e9512302de0a5711966
bibibobobi <smilingcat33@gmail.com>
2024-04-18 13:56:15 +0800

    feat(tv-next): add ui-500 component

--- packages/mirror-tv-next/components/errors/ui-500.tsx
+++ packages/mirror-tv-next/components/errors/ui-500.tsx
@@ -1,3 +1,16 @@
+import styles from '~/styles/components/errors/ui-500.module.scss'
 export default function Ui500() {
-  return <div>500</div>
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.error_info}>
+          <h1 className={styles.error_info_heading}>
+            50<span className={styles.drop_zero}>0</span>
+          </h1>
+
+          <p className="error_info_text">這個網頁無法正常運作</p>
+        </div>
+      </div>
+    </div>
+  )
 }
