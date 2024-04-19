8f26172051c1cbae1891549121c89307ad836acf
YiHsuan <v61265@gmail.com>
2024-04-19 15:23:08 +0800

    refactor(tv-next): add console for debug error page

--- packages/mirror-tv-next/app/topic/error.tsx
+++ packages/mirror-tv-next/app/topic/error.tsx
@@ -11,7 +11,7 @@ export default function Error({
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
+    console.error({ error })
   }, [error])
 
   return <div>{error.message === 'NEXT_NOT_FOUND' ? <Ui404 /> : <Ui500 />}</div>
