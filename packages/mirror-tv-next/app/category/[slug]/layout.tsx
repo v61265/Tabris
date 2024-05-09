f02196b6cccf401a44a9a082bd7cfb4f400d5a14
YiHsuan <v61265@gmail.com>
2024-05-09 18:59:42 +0800

    refactor(tv-next): remove useless gpt-ad of category page

--- packages/mirror-tv-next/app/category/[slug]/layout.tsx
+++ packages/mirror-tv-next/app/category/[slug]/layout.tsx
@@ -1,8 +1,5 @@
 import styles from '~/styles/pages/category-layout.module.scss'
 import CategoryPageLayoutAside from '~/components/category/layout/aside'
-import dynamic from 'next/dynamic'
-const GPTAd = dynamic(() => import('~/components/ads/gpt/gpt-ad'))
-import { GPTPlaceholderDesktop } from '~/components/ads/gpt/gpt-placeholder'
 
 export default async function CategoryPageLayout({
   children,
@@ -11,10 +8,6 @@ export default async function CategoryPageLayout({
 }) {
   return (
     <main>
-      <GPTPlaceholderDesktop>
-        <p>廣告</p>
-        <GPTAd pageKey="all" adKey="PC_HD" />
-      </GPTPlaceholderDesktop>
       <section className={styles.category}>
         {children}
         <CategoryPageLayoutAside />
