4857436e89ee48bea43b86285eeff89fd9b202c7
YiHsuan <v61265@gmail.com>
2024-06-03 14:08:28 +0800

    refactor(tv-next/ui-aside-videos-list): ajust logic of handlePlaying

--- packages/mirror-tv-next/components/shared/ui-aside-videos-list.tsx
+++ packages/mirror-tv-next/components/shared/ui-aside-videos-list.tsx
@@ -4,6 +4,7 @@ import styles from './_styles/ui-aside-video-list.module.scss'
 import UiHeadingBordered from './ui-heading-bordered'
 import YoutubeEmbed from './youtube-embed'
 import { extractYoutubeId } from '~/utils'
+import { YouTubeEvent } from 'react-youtube'
 
 type UiAsideVideosListProps = {
   title: string
@@ -19,14 +20,20 @@ export default function UiAsideVideosList({
   firstPlayTriggerClassName,
 }: UiAsideVideosListProps) {
   const triggeredDiv = useRef<HTMLDivElement>(null)
-  const [playTime, setPlayTime] = useState(0)
-  const handlePlaying = useCallback(() => {
-    if (!firstPlayTriggerClassName || playTime) return
-    setPlayTime((prev) => prev + 1)
-    if (triggeredDiv.current) {
-      triggeredDiv.current.click()
-    }
-  }, [playTime])
+  const [playedIds, setPlayIds] = useState<(number | never)[]>([])
+  const handlePlaying = useCallback(
+    (e: YouTubeEvent<number>) => {
+      if (!firstPlayTriggerClassName || !e) return
+      const targetId = e.target?.id
+      if (targetId && !playedIds.includes(targetId)) {
+        if (triggeredDiv.current) {
+          triggeredDiv.current.click()
+        }
+        setPlayIds((prev) => [...prev, e.target.id])
+      }
+    },
+    [playedIds]
+  )
   return (
     <>
       <div
@@ -45,7 +52,7 @@ export default function UiAsideVideosList({
               key={video.id}
               youtubeId={youtubeId}
               autoplay={isAutoPlay}
-              muted={true}
+              muted={isAutoPlay}
               loop={true}
               controls={true}
               handlePlaying={handlePlaying}
