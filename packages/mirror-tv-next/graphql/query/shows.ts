110345db7bf622f0c956b16ea692794037a18f76
YiHsuan <v61265@gmail.com>
2024-06-14 00:13:26 +0800

    feat(tv-next): add show page

--- packages/mirror-tv-next/graphql/query/shows.ts
+++ packages/mirror-tv-next/graphql/query/shows.ts
@@ -1,14 +1,35 @@
 import gql from 'graphql-tag'
+import { HeroImage } from '~/types/common'
 
 export type Show = {
   id: string
   slug: string
   name: string
-  bannerImg: {
-    urlMobileSized: string
-    urlTabletSized: string
-    urlOriginal: string
-  } | null
+  bannerImg: HeroImage | null
+}
+
+export type HostOrStaff = {
+  slug: string
+  name: string | null
+  sortOrder: number | null
+  bioApiData: string | null
+  showhostImg?: HeroImage | null
+  anchorImg?: HeroImage | null
+}
+
+export type ShowWithDetail = {
+  slug: string | null
+  name: string | null
+  bannerImg: HeroImage | null
+  picture: HeroImage | null
+  hostName?: HostOrStaff[] | null
+  staffName?: HostOrStaff[] | null
+  introduction: string | null
+  facebookUrl: string | null
+  igUrl: string | null
+  playList01: string | null
+  playList02: string | null
+  trailerPlaylist: string | null
 }
 
 const fetchShows = gql`
@@ -25,4 +46,72 @@ const fetchShows = gql`
     }
   }
 `
-export { fetchShows }
+
+const fetchShowBySlug = gql`
+  query fetchShowBySlug(
+    $slug: String!
+    $shouldFetchHost: Boolean = false
+    $shouldFetchStaff: Boolean = false
+    $squareHostImg: Boolean = false
+    $rectHostImg: Boolean = false
+  ) {
+    allShows(where: { slug: $slug }) {
+      slug
+      name
+      bannerImg {
+        urlDesktopSized
+        urlMobileSized
+        urlTabletSized
+        urlOriginal
+      }
+      picture {
+        urlDesktopSized
+        urlMobileSized
+        urlTabletSized
+        urlOriginal
+      }
+      hostName(sortBy: [sortOrder_ASC, id_DESC])
+        @include(if: $shouldFetchHost) {
+        slug
+        name
+        sortOrder
+        bioApiData
+        showhostImg @include(if: $squareHostImg) {
+          urlMobileSized
+          urlTabletSized
+          urlOriginal
+        }
+        anchorImg @include(if: $rectHostImg) {
+          urlMobileSized
+          urlTabletSized
+          urlOriginal
+        }
+      }
+      staffName(sortBy: [sortOrder_ASC, id_DESC])
+        @include(if: $shouldFetchStaff) {
+        slug
+        name
+        sortOrder
+        bioApiData
+        showhostImg @include(if: $squareHostImg) {
+          urlMobileSized
+          urlTabletSized
+          urlOriginal
+        }
+        anchorImg @include(if: $rectHostImg) {
+          urlMobileSized
+          urlTabletSized
+          urlOriginal
+        }
+      }
+      introduction
+      facebookUrl
+      igUrl
+      playList01
+      playList02
+      trailerPlaylist
+    }
+  }
+`
+
+export { fetchShows, fetchShowBySlug }
