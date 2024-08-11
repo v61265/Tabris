5e614fe80ddd774c847a4305a6ebd50a3719a684
YiHsuan <v61265@gmail.com>
2024-08-11 21:37:12 +0800

    refactor(tv-next/main-header): add default value of header data

--- packages/mirror-tv-next/components/layout/header/main-header.tsx
+++ packages/mirror-tv-next/components/layout/header/main-header.tsx
@@ -50,7 +50,11 @@ export default async function MainHeader() {
   let categoriesData: Category[] = []
   let showsData: Show[] = []
 
-  const { allCategories, allSponsors, allShows } = await getData()
+  const {
+    allCategories = [],
+    allSponsors = [],
+    allShows = [],
+  } = await getData()
 
   categoriesData = allCategories
   sponsorsData = allSponsors
