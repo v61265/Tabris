81ee9224cac52e6b2f94eaeed2c153daffd5feea
LinHeMa <linhema0921@gmail.com>
2024-12-25 10:42:32 +0800

    fix(story): delete unused code

--- packages/mirror-tv-next/components/story/article-social-list.tsx
+++ packages/mirror-tv-next/components/story/article-social-list.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import styles from './_styles/article-social-list.module.scss'
-import Link from 'next/link'
 import { socialMediaConfig, socialMediaOrder } from '~/constants/social-medial'
 
 const ArticleSocialList = () => {
@@ -14,34 +13,19 @@ const ArticleSocialList = () => {
           const imageSize = 50
           const defaultImageSrc = '/images/image-default.jpg'
 
-          if (!socialMediaData) {
-            // 若無對應資料，使用預設圖片
-            const defaultImageConfig = {
-              alt: socialMedia,
-              src: defaultImageSrc,
-              width: imageSize,
-              height: imageSize,
-            }
-            return (
-              <li key={socialMedia}>
-                <Image {...defaultImageConfig} />
-              </li>
-            )
-          }
-
           const { image, href } = socialMediaData
           const imageConfig = {
             alt: socialMedia,
             src: image || defaultImageSrc,
             width: imageSize,
             height: imageSize,
-          }
+          } satisfies Parameters<typeof Image>[0]
 
           return (
             <li key={socialMedia}>
-              <Link href={href}>
+              <a href={href}>
                 <Image {...imageConfig} />
-              </Link>
+              </a>
             </li>
           )
         })}
