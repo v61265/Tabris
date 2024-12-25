7db674d98c24235ceac695629d071a2dd3cca660
LinHeMa <linhema0921@gmail.com>
2024-12-25 15:14:10 +0800

    fix: gql name changed

--- packages/mirror-tv-next/components/story/article-related-posts.tsx
+++ packages/mirror-tv-next/components/story/article-related-posts.tsx
@@ -1,12 +1,12 @@
 import UiHeadingBordered from '../shared/ui-heading-bordered'
-import { Post } from '~/graphql/query/story'
+import { SinglePost } from '~/graphql/query/story'
 
 import styles from './_styles/article-related-posts.module.scss'
 
 const ArticleRelatedPosts = ({
   relatedPosts,
 }: {
-  relatedPosts: Post['relatedPosts']
+  relatedPosts: SinglePost['relatedPosts']
 }) => {
   return (
     <div className={styles.container}>
