b2659778fc7b49f8294bec59a364a8113e57837b
LinHeMa <linhema0921@gmail.com>
2024-12-25 14:29:13 +0800

    fix: remove unused gql type

--- packages/mirror-tv-next/graphql/query/story.ts
+++ packages/mirror-tv-next/graphql/query/story.ts
@@ -1,78 +1,16 @@
 import gql from 'graphql-tag'
-export interface HeroImage {
-  original: string
-  tiny: string
-  mobile: string
-  tablet: string
-  desktop: string
-  urlOriginal: string
-}
 
-export interface HeroVideo {
-  youtubeUrl: string
-}
-
-export interface Category {
-  slug: string
-  title: string
-}
-
-export interface Contributor {
-  name: string
-  slug: string
-}
-
-export interface Tag {
-  name: string
-}
-
-export interface RelatedPost {
+export interface SingleRelatedPost {
   slug: string
   name: string
-  heroImage: {
-    tiny: string
-    mobile: string
-    tablet: string
-    desktop: string
-    urlOriginal: string
-  }
 }
 
-export interface Download {
-  id: string
-  title: string
-  url: string
-}
-
-export interface Post {
-  style: string
-  state: string
-  title: string
-  brief: string
-  briefApiData: string
-  contentApiData: string
-  publishTime: string // Date in ISO format
-  updatedAt: string // Date in ISO format
-  source: string
-  isAdult: boolean
-  heroImage: HeroImage
-  heroVideo?: HeroVideo
-  heroCaption: string
-  categories: Category[]
-  writers: Contributor[]
-  photographers: Contributor[]
-  cameraOperators: Contributor[]
-  designers: Contributor[]
-  engineers: Contributor[]
-  vocals: Contributor[]
-  otherbyline: string
-  tags: Tag[]
-  relatedPosts: RelatedPost[]
-  download: Download[]
+export interface SinglePost {
+  relatedPosts: SingleRelatedPost[]
 }
 
 export interface FetchStoryBySlugResponse {
-  allPosts: Post[]
+  allPosts: SinglePost[]
 }
 
 const fetchStoryBySlug = gql`
